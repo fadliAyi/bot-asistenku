ca628bce596b562b3538598d114820744d771c65
fadliAyi <m.fadli4545@gmail.com>
2021-07-17 17:58:25 +0800

    change url

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ const app = require('express')();
 const {Telegraf} = require('telegraf');
 const bot = new Telegraf('1796040437:AAEDgh10xOSvXGL-ibmt9pTJBwmSjafzh4k');
 
-app.get('test-server', (req, res) => {
+app.get('/api/test-server', (req, res) => {
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.json({status: true});
 });
 
-app.post('webhook', (req,res) => {
+app.post('/api/webhook', (req,res) => {
     bot.hears('hello', ctx => {
         ctx.reply('hello from webhook');
     });
